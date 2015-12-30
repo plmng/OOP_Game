e7d8200786656f2518cea2e007f3331864db7744
Plamena Georgieva <plmng13@gmail.com>
2015-12-30 23:43:05 +0200

    hw1

---
+++ 02_Naming_Identifiers/02_Naming_Identifiers_Homework/Application2/Minesweeper.cs
@@ -0,0 +1,13 @@
+﻿namespace Application2
+{
+    using Models;
+
+    public class Minesweeper
+    {
+        private static void Main()
+        {
+            var engine = new Engine();
+            engine.Run();
+        }
+    }
+}
